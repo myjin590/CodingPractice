d54e45ea8cd3642e6bd36ca73b073e593548e4ca
Jinny Yun <85202828+myjin590@users.noreply.github.com>
2023-12-06 10:51:21 -0500

    CodingQuestion/FlippingBits-Solutions

---
+++ FlippingBits.cpp
@@ -0,0 +1,6 @@
+//Q. You will be given a list of 32 bit unsigned integers. Flip all the bits ( and ) and return the result as an unsigned integer.
+
+long flippingBits(long n) {
+    unsigned int b = ~n;
+    return b;
+}
