1c1020acdd106f81f66288cecf21475c1d284637
Jinny Yun <85202828+myjin590@users.noreply.github.com>
2024-04-24 21:10:13 -0400

    CodingPractice/58.LengthofLastWord-Solution
    58.LengthofLastWord-Solution

---
+++ TopInterview150/58.LengthofLastWord.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        for(int i=s.length()-1; i >= 0; i--){
+            if(count == 0 && s[i] == ' '){
+                continue;
+            }
+            else if(s[i] != ' '){
+                count++;
+            }
+            else if(count != 0 && s[i] == ' '){
+                break;
+            }
+        }
+        return count;
+    }
+};
